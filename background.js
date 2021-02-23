9815faa9b1fb549cbc56b96358237da078bc7c83
Conor Watson <conor@odro.co.uk>
2021-02-23 16:46:35 +0000

    fix: background.js now executes initialiseExtension

--- background.js
+++ background.js
@@ -1,5 +1,5 @@
 chrome.browserAction.onClicked.addListener(function (tab) {
   chrome.tabs.executeScript(tab.ib, {
-    file: 'src/initialiseRecorder.js'
+    file: 'src/initialiseExtension.js'
   });
 });
