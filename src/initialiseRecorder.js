f398a936c41f9615b05fcbbd39b06d335cfc7f90
Conor Watson <conor@odro.co.uk>
2021-02-23 13:36:45 +0000

    feat: converted media-viewer into a Chrome Extension

---
+++ src/initialiseRecorder.js
@@ -0,0 +1,48 @@
+const defaultConstraints = {
+  audio: true,
+  video: {
+    width: { min: 320, ideal: 1024, max: 2048 },
+    height: { min: 240, ideal: 768, max: 1536 },
+    facingMode: 'user',
+  },
+};
+
+const getUserMediaStream = async (constraints) => {
+  try {
+    const stream = await navigator.mediaDevices.getUserMedia(constraints);
+
+    return stream;
+  } catch (err) {
+    console.log('The following getUserMedia error occured:.. ', err);
+    console.log({ code: err.code, name: err.name, message: err });
+    throw err;
+  }
+}
+
+const initiateMediaViewer = async (data) => {
+  try {
+    console.log('Initiating Media Viewer')
+    const video = document.querySelector('video');
+    const stream = await getUserMediaStream(defaultConstraints);
+    console.log({ stream, video })
+    video.muted = true;
+    video.srcObject = stream;
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+const injectUI = async () => {
+  const screenRecorder = await fetch(chrome.runtime.getURL('src/screen-recorder.html'));
+  const screenRecorderHTML = await screenRecorder.text();
+  
+  document.head.insertAdjacentHTML('beforeend', '<link href="' + chrome.runtime.getURL('src/screen-recorder.css') + '" rel="stylesheet">')
+  document.body.insertAdjacentHTML('beforeend', screenRecorderHTML);
+}
+
+const initialiseExtension = async () => {
+  await injectUI();
+  initiateMediaViewer();
+}
+
+initialiseExtension();
