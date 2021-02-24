ef00f6f5f0dd7b7f147b52a0ea14afa51efe88aa
Conor Watson <conor@odro.co.uk>
2021-02-24 15:04:06 +0000

    feat: moved stream recording to background.js

--- background.js
+++ background.js
@@ -1,5 +1,89 @@
+const displayMediaOptions = {
+  video: true,
+  audio: true,
+};
+
+const chunks = [];
+let mediaRecorder = null;
+
 chrome.browserAction.onClicked.addListener(function (tab) {
   chrome.tabs.executeScript(tab.ib, {
     file: 'src/initialiseExtension.js'
   });
 });
+
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  if (request.action === 'testAction') {
+    console.log('test success!')
+    return sendResponse(`The date you sent was: ${request.payload}`)
+  }
+  else if (request.action === 'startRecording') {
+    startRecording(request, sender, sendResponse)
+  }
+  else if (request.action === 'stopRecording') {
+    stopCapture();
+
+    setTimeout(() => {
+      const url = URL.createObjectURL(new Blob(chunks));
+      console.log('final video URL', url);
+      sendResponse({ url })
+    }, 5000)
+  }
+
+  return true;
+});
+
+const startRecording = async (request, sender, sendResponse) => {
+  const { isTabCapture } = request.payload;
+
+  if(isTabCapture){
+    const stream = await startTabCapture();
+    if(stream) recordStream(stream);
+  } 
+  else {
+    const stream = await startCapture();
+    if(stream) recordStream(stream);
+  }
+}
+
+const startCapture = async () => {
+  console.log('Start Capture')
+  let capturedStream = null;
+
+  try {
+    capturedStream = await navigator.mediaDevices.getDisplayMedia(displayMediaOptions);
+  } catch(err) {
+    console.error("Error: " + err);
+  }
+
+  return capturedStream
+}
+
+const startTabCapture = () => {
+  console.log('Start Tab Capture');
+
+  return new Promise((resolve, reject) => {
+    chrome.tabCapture.capture(displayMediaOptions, stream => {
+      if(stream) return resolve(stream);
+      else return reject('Error: no stream provided by tabCapture')
+    });
+  });
+}
+
+const stopCapture = () => {
+  console.log('Stop Capturing')
+
+  mediaRecorder.stop();
+}
+
+const recordStream = (stream) => {
+  const options = { mimeType: 'video/webm;codecs=vp8' };
+  mediaRecorder = new MediaRecorder(stream, options);
+  mediaRecorder.ondataavailable = (event) => {
+    if (event.data.size > 0) {
+      console.log('saving chunk', event.data)
+      chunks.push(event.data);
+    }
+  };
+  mediaRecorder.start(1000);
+}
--- src/mediaRecorder.js
+++ src/mediaRecorder.js
@@ -1,60 +1,32 @@
-const displayMediaOptions = {
-  video: true,
-  audio: true,
-};
-
-const chunks = [];
-let mediaRecorder = null;
-
-async function startCapture() {
-  console.log('Start Capturing')
-  let capturedStream = null;
-
+const downloadFromUrl = (url) => {
   try {
-    capturedStream = await navigator.mediaDevices.getDisplayMedia(displayMediaOptions);
-  } catch(err) {
-    console.error("Error: " + err);
+    const x = new XMLHttpRequest();
+    x.open('GET', url);
+    x.responseType = 'blob';
+    x.onload = function () {
+      const url = URL.createObjectURL(x.response);
+      
+      const a = document.createElement('a');
+      document.body.appendChild(a);
+      a.style = 'display: none';
+      a.href = url;
+      a.download = 'test.webm';
+      a.click();
+      window.URL.revokeObjectURL(url);
+    };
+    x.send();
+  } catch (e) {
+    console.log('Failed to download blobURL', e);
   }
-  return capturedStream
-}
-
-function recordStream(stream) {
-  const options = { mimeType: 'video/webm;codecs=vp8' };
-  mediaRecorder = new MediaRecorder(stream, options);
-  mediaRecorder.ondataavailable = (event) => {
-    if (event.data.size > 0) {
-      console.log('saving chunk', event.data)
-      chunks.push(event.data);
-    }
-  };
-  mediaRecorder.start(1000);
-}
-
-function stopCapture(evt) {
-  console.log('Stop Capturing')
-  mediaRecorder.stop();
-  
-  download(chunks);
-}
-
-function download(recordedChunks) { 
-  var blob = new Blob(recordedChunks, {
-    type: 'video/webm'
-  });
-  var url = URL.createObjectURL(blob);
-  var a = document.createElement('a');
-  document.body.appendChild(a);
-  a.style = 'display: none';
-  a.href = url;
-  a.download = 'test.webm';
-  a.click();
-  window.URL.revokeObjectURL(url);
 }
 
 const onStopClick = () => {
   const button = document.querySelector('#sc-button');
 
-  stopCapture();
+  chrome.runtime.sendMessage({ action: 'stopRecording' }, response => { 
+    console.log('Recieved URL: ', response.url);
+    downloadFromUrl(response.url)
+  });
 
   button.classList.remove('recording');
   button.innerText = "Start Recording";
@@ -66,14 +38,14 @@ const onStopClick = () => {
 const onStartClick = async () => {
   const button = document.querySelector('#sc-button');
 
-  const stream = await startCapture();
-  if(stream) recordStream(stream);
+  chrome.runtime.sendMessage({ action: 'startRecording', payload: { isTabCapture: false } });
 
   button.classList.add('recording');
   button.innerText = "Stop Recording";
 
   button.removeEventListener('click', onStartClick);
   button.addEventListener('click', onStopClick);
+
 }
 
 
